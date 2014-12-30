318341416d14d5da85c04fc5394a8e5dd0868e75
Sundarasan <sundarasan.natarajan@a-cti.com>
2014-12-30 12:14:40 +0530

    Deleted Unwanted JAR files

--- src/com/sujsun/cms/Sujsun_CMSServlet.java
+++ src/com/sujsun/cms/Sujsun_CMSServlet.java
@@ -1,13 +1,18 @@
 package com.sujsun.cms;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
+
+import org.mortbay.log.Log;
 
 @SuppressWarnings("serial")
 public class Sujsun_CMSServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		String returnMessage = "Hello, world";
+		System.out.println( returnMessage );
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println( returnMessage );
 	}
 }
